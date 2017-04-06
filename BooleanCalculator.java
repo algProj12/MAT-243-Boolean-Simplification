746ac4f7ca14a7e149f5264a8160bcc701d65ca8
algProj12 <dplotzke@asu.edu>
2017-04-05 22:05:11 -0700

    addition of new parseEquation function
    this separate version of the function parses only one row in a Boolean table. I also removed the version of parseEquation that parsed number operations which we shouldn't need.

--- BooleanCalculator.java
+++ BooleanCalculator.java
@@ -56,6 +56,42 @@ public class BooleanCalculator {
         
     }
     
+    private static boolean[] parseEquation(boolean[] refTable, int intVar, 
+            String input){
+        
+        final Stack<Boolean> boolStack = new Stack<>();
+        
+        
+        for(int j = 0; j < input.length(); j++){
+            int counter = 0;
+            char chara;
+            boolean first;
+            boolean second;
+            chara = input.charAt(j);
+
+            switch(chara){
+                case '+':
+                    first = boolStack.pop();
+                    second = boolStack.pop();
+                    boolStack.push(first || second);
+                break;
+                case '*':
+                    first = boolStack.pop();
+                    second = boolStack.pop();
+                    boolStack.push(first && second);
+                break;
+                case '!':
+                    boolStack.push(!boolStack.pop());
+                break;
+                default:
+                    boolStack.push(refTable[counter]);
+                    counter++;
+            }
+        }
+        refTable[intVar] = boolStack.pop();
+        return refTable;
+    }
+    
     /**
      * Description: determines the answers to a boolean equation of postfix
      * @param refTable
@@ -100,44 +136,4 @@ public class BooleanCalculator {
         return refTable;
     }
     
-    /**
-     * Description: determines the answer to a mathematical equation of postfix
-     * @param input
-     * @param intVar
-     * @return 
-     */
     
-    public static double parseEquation(String input, int intVar){
-        
-        final Stack<Double> doubleStack = new Stack<>();
-        
-        for(int i = 0; i < Math.pow(2, intVar); i++){
-            for(int j = 0; j < input.length(); j++){
-                char chara;
-                chara = input.charAt(j);
-                double topStack;
-
-                switch(chara){
-                    case '+':
-                        doubleStack.push(doubleStack.pop() + doubleStack.pop());
-                    break;
-                    case '*':
-                        doubleStack.push(doubleStack.pop()*doubleStack.pop());
-                    break;
-                    case '-':
-                        topStack = doubleStack.pop();
-                        doubleStack.push(doubleStack.pop()-topStack);
-                    break;
-                    case '/':
-                        topStack = doubleStack.pop();
-                        
-                        doubleStack.push(doubleStack.pop()/topStack);
-                    break;
-                    default:
-                        doubleStack.push(Double.parseDouble(Character.toString(chara)));
-                }
-            }
-        }
-        return doubleStack.pop();
-    }
-}
